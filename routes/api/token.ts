dbf59f82e73ebdb32a57dcc32cfa35bd6873c36b
bstafford2017 <b.stafford2017@gmail.com>
2020-10-17 10:44:43 -0500

    Convert Token route

--- routes/api/token.ts
+++ routes/api/token.ts
@@ -1,20 +1,21 @@
 import jwt from 'jsonwebtoken'
 import express from 'express'
-import log from '../../utils/log'
+import { log } from '../../utils'
+import { IUser } from '../../client/src/types'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    const token = req.cookies.token
+    const token: string = req.cookies.token
     if (token) {
-      const result = await jwt.verify(token, 'secret-key')
+      const result: IUser = await jwt.verify(token, 'secret-key')
       res.json({ token: true })
     } else {
       res.json({ token: false })
     }
   } catch (err) {
-    log('error-log', err.tostring() + '\n')
+    log('error-log', err.toString() + '\n')
     res.json({ token: false })
   }
 })
