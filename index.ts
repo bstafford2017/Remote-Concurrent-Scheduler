c77ddce1ec593eb03c8413aef17aed6d71d9d675
bstafford2017 <b.stafford2017@gmail.com>
2020-11-15 14:37:53 -0600

    Remove utils and old dependencies

--- index.ts
+++ index.ts
@@ -3,36 +3,21 @@ import mongoose from 'mongoose'
 import { graphqlHTTP } from 'express-graphql'
 import schema from './schema'
 import config from './config'
-import { logger } from './utils'
-import tokenRoutes from './routes/api/token'
-import eventRoutes from './routes/api/event'
-import userRoutes from './routes/api/user'
-import buildingRoutes from './routes/api/building'
-import roomRoutes from './routes/api/room'
+;(async () => {
+  try {
+    const app: express.Application = express()
 
-const app: any = express()
+    await mongoose.connect(config.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
 
-mongoose
-  .connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(() => console.log('MongoDB connected...'))
-  .catch((err) => console.error(err))
+    app.use('/graphiql', graphqlHTTP({ schema, graphiql: true }))
 
-app.use('/graphiql', graphqlHTTP({ schema, graphiql: true }))
-
-// Middleware for body parsing for POST
-app.use(express.urlencoded({ extended: true }))
-
-// Middleware for parsing JSON
-app.use(express.json())
-
-// Middleware for logging requests
-app.use(logger)
-
-let port = process.env.PORT || 5000
-
-// Listen for request
-app.listen(port, () => console.log(`Server started on port ${port}`))
+    let port = process.env.PORT || 5000
+    app.listen(port, () => console.log(`Server started on port ${port}`))
+  } catch (e) {
+    console.error(e)
+  }
+})()
